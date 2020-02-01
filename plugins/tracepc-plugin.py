edbf2f154392f5f2eb01f99e576d2f9c87de045f
alset0326 <alset0326@gmail.com>
2020-02-01 14:12:14 +0800

    add trace pc plugin

---
+++ plugins/tracepc-plugin.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+
+from peda.utils import *
+
+
+def enable_log(peda, filename):
+    peda.execute('set logging off')  # prevent nested call
+    peda.execute('set height 0')  # disable paging
+    peda.execute('set logging file %s' % filename)
+    peda.execute('set logging overwrite on')
+    peda.execute('set logging redirect on')
+    peda.execute('set logging debugredirect on')
+    peda.execute('set logging on')
+
+
+def disable_log(peda):
+    peda.execute('set logging off')
+
+
+def invoke(peda, *arg):
+    """
+    Print pc until target pc
+    Usage:
+        sample target-pc [out-file]
+    """
+
+    (target_pc, filename) = normalize_argv(arg, 2)
+    target_pc = to_int(target_pc)
+    if target_pc is None:
+        raise Exception()
+
+    peda.save_user_command("hook-stop")  # disable hook-stop to speedup
+    info("Stepping through, Ctrl-C to stop...")
+    if filename is not None:
+        f = open(filename, 'wt')
+    else:
+        f = sys.stdout
+
+    try:
+        cur = peda.getpc()
+        while cur != target_pc:
+            print(hex(cur), file=f)
+            peda.execute("stepi")
+            cur = peda.getpc()
+    finally:
+        if filename is not None:
+            f.close()
+        peda.restore_user_command("hook-stop")
+
+
+invoke.options = []
